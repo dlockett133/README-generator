221bd5e49173cbf3e3a27ceb0b3c9eb6751ca5f8
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-01-07 02:34:31 -0500

    modifed questions variable to be an array, and inserted questions into 'init' func

--- index.js
+++ index.js
@@ -2,33 +2,67 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 // TODO: Create an array of questions for user input
-// const questions = [`What is the title of your project?`,`A Description of your project?`, `What are the installation instructions (if none, type "N/A")?`, `Usage information?`, `Contribution Guidelines?`, `Test Instructions?`];
-const questions = {
-    "title":`What is the title of your project?`,
-    "description":`A Description of your project?`,
-    "installation": `What are the installation instructions (if none, type "N/A")?`,
-    "usage": `Usage information?`, 
-    "contributions":`Contribution Guidelines?`,
-    "test": `Test Instructions?`
-}
+const questions = [
+    `What is the title of your project?`,
+    `A Description of your project?`, 
+    `What are the installation instructions (if none, type "N/A")?`, 
+    `Usage information?`, 
+    `Contribution Guidelines?`, 
+    `Test Instructions?`
+];
+
+// const questions = {
+//     "title":`What is the title of your project?`,
+//     "description":`A Description of your project?`,
+//     "installation": `What are the installation instructions (if none, type "N/A")?`,
+//     "usage": `Usage information?`, 
+//     "contributions":`Contribution Guidelines?`,
+//     "test": `Test Instructions?`
+// }
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    for(const question in questions){
-        // console.log(question)
+        // console.log(questions[0])
         inquirer
             .prompt([
                 {
-                    type: `${question}`,
-                    name: `${question}`,
-                    message: ``
-                }
+                    type: `input`,
+                    name: `title`,
+                    message: `${questions[0]}`
+                },
+                {
+                    type: `input`,
+                    name: `description`,
+                    message: `${questions[1]}`
+                },
+                {
+                    type: `input`,
+                    name: `installation`,
+                    message: `${questions[2]}`
+                },
+                {
+                    type: `input`,
+                    name: `usage`,
+                    message: `${questions[3]}`
+                },
+                {
+                    type: `input`,
+                    name: `contributions`,
+                    message: `${questions[4]}`
+                },
+                {
+                    type: `input`,
+                    name: `test`,
+                    message: `${questions[5]}`
+                },
             ])
+            .then((data)=>{
+                console.log(data)
+            })
     }
-}
 
 // Function call to initialize app
 init();
