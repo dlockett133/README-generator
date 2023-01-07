bd687378b410d2de9626e3f2d6df2551fd943c33
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-01-07 01:20:26 -0500

    insert questions into array

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [`What is the title of your project?`,`A Description of your project`, `What are the installation instructions (if none, type "N/A")?`, `Usage information?`, `Contribution Guidelines?`, `Test Instructions?`];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
