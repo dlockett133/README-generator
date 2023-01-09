537bbd9826aa40b580287019db6d63662c48e733
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-01-09 13:40:17 -0500

    modified generateMarkdown functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,26 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge = `https://img.shields.io/badge/License-${license}-blue.svg`
+  return badge
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let link = `https://opensource.org/licenses/${license}`
+  return link;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseSection(license) {
+  if (license === null) {
+    return ""
+  } else {
+    return `## License`
+  }
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
@@ -29,7 +40,7 @@ ${data.installation}
 ## Usage
 ${data.usage}
 
-##License
+## License
 ${data.license}
 
 ## Contributing
